8213d04aec3c5aebc88d2d58a0b116fb45a86c3f
alize1 <zeeali35@hotmail.com>
2018-12-27 16:51:58 -0500

    List Changes on Python

---
+++ meeting.py
@@ -0,0 +1,6 @@
+attendees = ["Ken", "Lisa", "Zeeshan"]
+attendees.append("Ashley")
+attendees.extend(["James","Gill"])
+optional_invitees = (["Ben", "Ali", "Clark"])
+potential_attendees = attendees + optional_invitees
+print ("There are", len(potential_attendees), "attendees currently")
---
+++ wishlist.py
@@ -0,0 +1,7 @@
+books = [
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
