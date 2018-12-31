39c3e5db402c06570507c75f30705d73b2865d6c
alize1 <zeeali35@hotmail.com>
2018-12-31 14:35:09 -0500

    List Changes on Python

--- meeting.py
+++ meeting.py
@@ -1,6 +1,11 @@
+
+
 attendees = ["Ken", "Lisa", "Zeeshan"]
 attendees.append("Ashley")
 attendees.extend(["James","Gill"])
 optional_invitees = (["Ben", "Ali", "Clark"])
 potential_attendees = attendees + optional_invitees
 print ("There are", len(potential_attendees), "attendees currently")
+
+for attendee in potential_attendees:
+    print (attendee)
